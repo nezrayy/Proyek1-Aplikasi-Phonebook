9628fa69f5f1abcf243e41afef29474ad66040a9
Rayhan Fanez Fathiadi <123948620+nezrayy@users.noreply.github.com>
2023-02-18 06:38:36 +0700

    Update

--- phonebook_221524027.py
+++ phonebook_221524027.py
@@ -1,69 +1,80 @@
 import os
-import fileinput
-import sys
 
 
 def tambah():
     os.system('cls')
-    tmb = open("kontak.txt", "a")
+    tmb = open("kontak.txt", 'a')
     tempNama = input("Masukkan nama: ")
     tempNomor = input("Masukkan nomor: ")
     tmb.write("{}\n".format(tempNama + "\t\t" + tempNomor))
     tmb.close()
     print("Kontak baru sudah berhasil ditambahkan!")
-    input()
 
 
 def tampilKontak():
-    if os.path.getsize('kontak.txt') == 0:
+    if os.path.getsize("kontak.txt") == 0:
         print("Daftar kontak kosong!")
     else:
         os.system('cls')
-        tmp = open("kontak.txt", "r")
-        print("Nama\t\tNomor\n=========================")
-        print(tmp.read())
+        tmp = open("kontak.txt", 'r')
+        print("Nama\t\tNomor\n=========================\n" + tmp.read())
+        # print(tmp.read())
         tmp.close()
 
 
 def hapus():
-    if os.path.getsize('kontak.txt') == 0:
+    if os.path.getsize("kontak.txt") == 0:
         print("Daftar kontak kosong!")
     else:
-        kontakDihapus = input("Masukkan nama yang akan dihapus: ")
-        with open('kontak.txt', 'r') as f:
-            lines = f.readlines()
-        isiBaru = [line for line in lines if kontakDihapus not in line]
-        with open('kontak.txt', 'w') as f:
-            f.writelines(isiBaru)
-        print("Kontak berhasil dihapus!")
+        tampilKontak()
+        with open("kontak.txt", 'r') as cek:
+            isi = cek.read()
+            kontakDihapus = input("Masukkan nama yang akan dihapus: ")
+        if kontakDihapus in isi:
+            with open("kontak.txt", 'r') as hps:
+                lines = hps.readlines()
+            isiBaru = [line for line in lines if kontakDihapus not in line]
+            with open("kontak.txt", 'w') as hps:
+                hps.writelines(isiBaru)
+            print("Kontak berhasil dihapus!")
+        else:
+            os.system('cls')
+            print(
+                "Kontak tidak tersedia, silahkan pilih nama yang tersedia pada daftar anda.")
 
-
-def edit():
-    if os.path.getsize('kontak.txt') == 0:
+def editKontak():
+    os.system('cls')
+    if os.path.getsize("kontak.txt") == 0:
         print("Daftar kontak kosong!")
     else:
-        namaDiedit = input("Masukkan nama yang akan diedit: ")
-        tempNamaBaru = input("Masukkan nama baru: ")
-        tempNomorBaru = input("Masukkan nomor baru: ")
-        for line in fileinput.input("kontak.txt", inplace=1):
-            if namaDiedit in line:
-                line = line.replace(line, tempNamaBaru +
-                                    "\t\t" + tempNomorBaru + "\n")
-                sys.stdout.write(line)
-        print("Kontak berhasil diedit!")
+        tampilKontak()
+        namaDiganti = input("Masukan nama yang diganti: ")
+        namaTemp = input("Masukan nama baru: ")
+        nomorTemp = input("Masukan nomor baru: ")
+        with open("kontak.txt", 'r') as edit:
+            lines = edit.readlines()
+        with open("kontak.txt", 'w') as edit:
+            for index, line in enumerate(lines):
+                if namaDiganti in line:
+                    lines[index] = namaTemp + "\t\t" + nomorTemp + '\n'
+            edit.writelines(lines)
+            print("Kontak berhasil diubah!")
 
 
 def cari():
-    if os.path.getsize('kontak.txt') == 0:
+    if os.path.getsize("kontak.txt") == 0:
         print("Daftar kontak kosong!")
     else:
-        with open('kontak.txt', 'r') as file:
-            # read all content of a file
-            isi = file.read()
-            # check if string present in a file
-            namaDicari = input("Cek nama : ")
-            if namaDicari in isi:
-                print("Kontak tersedia")
+        namaDicari = input("Cek nama : ")
+        with open("kontak.txt", 'r') as cr:
+            for line in cr:
+                if namaDicari in line:
+                    ada = True
+                else:
+                    ada = False
+            if ada == True:
+                os.system('cls')
+                print("Nama\t\tNomor\n=========================\n" + line.strip())
             else:
                 print("Kontak tidak tersedia")
 
@@ -71,29 +82,29 @@ def cari():
 # Main
 while True:
     try:
-            os.system('cls')
-            pilih = int(input("  Phonebook-mu\n\n1. Buat Kontak Baru\n2. Hapus Kontak\n3. Cari kontak\n4. Edit Kontak\n5. Daftar Kontak\n6. Keluar\nPilih: "))
-            if pilih == 1:
-                tambah()
-            elif pilih == 2:
-                tampilKontak()
-                hapus()
-                input()
-            elif pilih == 3:
-                cari()
-                input()
-            elif pilih == 4:
-                tampilKontak()
-                edit()
-                input()
-            elif pilih == 5:
-                tampilKontak()
-                input()
-            elif pilih == 6:
-                exit()
-            else:
-                print("Pilihan tidak sesuai! Ulangi.")
-                input()
-    except Exception:
+        os.system('cls')
+        pilih = int(input(
+            "    Phonebook-mu\n\n1. Buat Kontak Baru\n2. Hapus Kontak\n3. Cari kontak\n4. Edit Kontak\n5. Daftar Kontak\n6. Keluar\n\nPilih: "))
+        if pilih == 1:
+            tambah()
+            input()
+        elif pilih == 2:
+            hapus()
+            input()
+        elif pilih == 3:
+            cari()
+            input()
+        elif pilih == 4:
+            editKontak()
+            input()
+        elif pilih == 5:
+            tampilKontak()
+            input()
+        elif pilih == 6:
+            exit()
+        else:
             print("Pilihan tidak sesuai! Ulangi.")
+            input()
+    except Exception:
+        print("Pilihan tidak sesuai! Ulangi.")
 # endMain
